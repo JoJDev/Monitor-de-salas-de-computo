a7a3bbc24f2249f2dff6cd92cba3457f08412a9f
Jesus OJ <jojfreelance@gmail.com>
2021-06-07 11:41:14 -0500

    Commit de la aplicacion y sus vistas

---
+++ Monitor de salas de computo/Administrador.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Monitor_de_salas_de_computo
+{
+    /// <summary>
+    /// Lógica de interacción para Administrador.xaml
+    /// </summary>
+    public partial class Administrador : Window
+    {
+        public Administrador()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Monitor de salas de computo/InicioSesion.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Monitor_de_salas_de_computo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string usu = txt_usu.Text;
+            string pass = txt_psw.Password.ToString();
+
+            switch (usu)
+            {
+                case "admin":
+                    Monitor_de_salas_de_computo.Administrador.GetWindow().Show();
+                    break;
+
+                case "ayudante":
+                    break;
+
+                case "usuario":
+                    break;
+
+                default:
+                    MessageBox.Show("Usuario o contraseña incorrecta"," Error de ");
+                    break;
+            }
+        }
+    }
+}
