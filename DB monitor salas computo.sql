627131278744bbce98dd18e4e5d164568f952e0d
Jesus OJ <jojfreelance@gmail.com>
2021-06-09 16:20:31 -0500

    Nueva tabla y datos para el SQL de la BD

--- DB monitor salas computo.sql
+++ DB monitor salas computo.sql
@@ -1,4 +1,5 @@
-CREATE DATABASE if not exists monitor_sala_computo;
+DELETE DATABASE IF EXISTS monitor_salas_computo;
+CREATE DATABASE if not exists monitor_salas_computo;
 
 CREATE TABLE usuarios (
   usuario_id SERIAL PRIMARY KEY NOT NULL ,
@@ -19,32 +20,40 @@ CREATE TABLE salas (
   sala_ip_inicial VARCHAR(16) NULL,
   sala_ip_final VARCHAR(16) NULL,
   sala_gateway VARCHAR(16) NOT NULL,
-  sala_servidor VARCHAR(16) NULL
+  sala_servidor VARCHAR(16) 
   );
 
 CREATE TABLE computadoras (
   comp_id SERIAL PRIMARY KEY NOT NULL,
+  sala_id SMALLINT REFERENCES salas(sala_id), 
   comp_nombre VARCHAR(25) NOT NULL,
   comp_ip VARCHAR(15) NULL,
   comp_submascara VARCHAR(15) NULL,
-  comp_fecha_adquisicion DATETIME NOT NULL,
-  id_salas INT NOT NULL
+  comp_fecha_adquisicion TIMESTAMP  NOT NULL
   );
 
 CREATE TABLE registros (
   registro_id SERIAL PRIMARY KEY NOT NULL,
-  registro_fecha_inicio DATETIME NOT NULL,
+  usuario_id INT REFERENCES usuarios(usuario_id),
+  comp_id INT REFERENCES computadoras(comp_id),
+  registro_fecha_inicio TIMESTAMP NOT NULL,
   registro_duracion_tiempo TIME NOT NULL,
-  registro_tipo_desconexion VARCHAR(3) NOT NULL,
-  id_usuario INT NOT NULL,
-  id_computadoras INT NOT NULL
+  registro_tipo_desconexion VARCHAR(3) NOT NULL  
+  );
+
+  CREATE TABLE configuraciones(
+    conf_id SERIAL PRIMARY KEY NOT NULL,
+    sala_id SMALLINT REFERENCES salas(sala_id),
+    conf_tiempo_espera SMALLINT DEFAULT 5,
+    conf_perm_usb BOOLEAN DEFAULT 'false',
   );
     
     insert into usuarios(usuario_nombre,usuario_nickname,usuario_contrasena, usuario_tipo, usuario_numero_cuenta, usuario_carrera
 					   , usuario_fecha_inicio, usuario_fecha_nacimiento) values 
-                       ('jesus', 'admin', 'admin', '0', '1621057', 'ico', '1995-02-02', '199-03-03'),
-                       ('jesus2', 'ayudamte', 'ayudamte', '1', '1621057', 'ico', '1995-02-02', '199-03-03'),
-                       ('jesus3', 'usuario', 'usuario', '2', '1621057', 'ico', '1995-02-02', '199-03-03');
+                       ('jesus', 'admin', 'admin', '0', '1621057', 'ico', '2016-08-03', '1995-03-27'),
+                       ('jesus2', 'ayudante', 'ayudante', '1', '1621057', 'ico', '2016-08-03', '1995-03-27'),
+                       ('jesus3', 'usuario', 'usuario', '2', '1621057', 'ico', '2016-08-03', '1995-03-27');
     
-    insert into salas (`sala_nombre`,`sala_plantel`, `sala_ip_inicial`, `sala_ip_final`, `sala_gateway`, `sala_servidor`)
-  VALUES ("Lab1", "CU Ecatepec", "192.168.1.2", "192.168.1.150", "192.168.1.1", "192.168.1.254");
+    insert into salas (sala_nombre, sala_plantel, sala_ip_inicial, sala_ip_final, sala_gateway, sala_servidor)
+  VALUES ('Lab1', 'CU Ecatepec', '192.168.1.2', '192.168.1.50', '192.168.1.1', '192.168.1.254')
+  , ('Lab2', 'C.U. Ecatepec', '192.168.1.52', '192.168.1.100', '192.168.1.1', '192.168.1.254');
