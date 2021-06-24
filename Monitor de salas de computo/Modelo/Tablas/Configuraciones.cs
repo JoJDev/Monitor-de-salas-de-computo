bcf450800ab455d355a967e98b188c7dda57fb74
Jesus OJ <jojfreelance@gmail.com>
2021-06-24 17:42:14 -0500

    Clase configuraciones hecha

--- Monitor de salas de computo/Modelo/Tablas/Configuraciones.cs
+++ Monitor de salas de computo/Modelo/Tablas/Configuraciones.cs
@@ -4,28 +4,25 @@ namespace Monitor_de_salas_de_computo.Modelo
 {
     class Configuraciones
     {
-        //id, idSala, tiempoEnEspera, permitirUSB
-        private int _id, _idSala;
-        private DateTime _tiempoEnEspera, _tiempoDeActalizacion;
-        private bool _permitirUSB/*en desarrollo*/;
-
         public Configuraciones()
         {
         }
 
-        public Configuraciones(int id, int idSala, DateTime tiempoEnEspera, DateTime tiempoDeActalizacion, bool permitirUSB)
+        public Configuraciones(int id, int idSala, DateTime tiempoEnEspera, DateTime tiempoDeActalizacion, bool permitirUSB, int numRegistrosMostrados)
         {
-            Id = id;
+            ConfId = id;
             IdSala = idSala;
             TiempoEnEspera = tiempoEnEspera;
             TiempoDeActalizacion = tiempoDeActalizacion;
             PermitirUSB = permitirUSB;
+            NumRegistrosMostrados = numRegistrosMostrados;
         }
 
-        public int Id { get => _id; set => _id = value; }
-        public int IdSala { get => _idSala; set => _idSala = value; }
-        public DateTime TiempoEnEspera { get => _tiempoEnEspera; set => _tiempoEnEspera = value; }
-        public bool PermitirUSB { get => _permitirUSB; set => _permitirUSB = value; }
-        public DateTime TiempoDeActalizacion { get => _tiempoDeActalizacion; set => _tiempoDeActalizacion = value; }
+        public int ConfId { get; set; }
+        public int IdSala { get; set; }
+        public DateTime TiempoEnEspera { get; set; }
+        public bool PermitirUSB { get; set; }
+        public DateTime TiempoDeActalizacion { get; set; }
+        public int NumRegistrosMostrados { get; set; }
     }
 }
