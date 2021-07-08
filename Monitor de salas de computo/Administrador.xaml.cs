f367c2f0ad216898f0146abbeaa360b8855bcd88
Jesus OJ <jojfreelance@gmail.com>
2021-07-08 13:17:38 -0500

    Creando la insersion, edicion y eliminacion de una tabla (registro)

--- Monitor de salas de computo/Administrador.xaml.cs
+++ Monitor de salas de computo/Administrador.xaml.cs
@@ -27,11 +27,13 @@ namespace Monitor_de_salas_de_computo
         IEnumerable<Sala> _salas;
         IEnumerable<Configuraciones> _configuraciones;*/
         AdministradorControl controlador;
+        List<CeldaControl> celdas;
 
         public Administrador()
         {
             InitializeComponent();
             controlador = new AdministradorControl();
+            celdas = new List<CeldaControl>();
             /*_registros = new RegistroORM().GetAll();
             dg_Registros.DataContext = _registros;
             _usuarios = new UsuarioORM().GetAll();
@@ -159,10 +161,33 @@ namespace Monitor_de_salas_de_computo
 
         private void dg_Registros_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
         {
-            MessageBox.Show($"{e.Row.GetIndex()} es el id de objeto actualizado\n" +
+            int NumFila = e.Row.GetIndex();
+            // Codigo para obtener el valor de la celda editada a travez del enumerador Registro
+            //string valorInicialCelda = controlador.Registros.ElementAt(NumFila).getPropiedadNum(e.Column.DisplayIndex).ToString();
+
+            /*MessageBox.Show($"{e.Row.GetIndex()} es el id de objeto actualizado\n" +
                 $" y la celda es : {controlador.Registros.ToArray()[e.Row.GetIndex()]}" +
-                $" columna: { e.Column.DisplayIndex}");
-            
+                $" columna: { e.Column.DisplayIndex} and = {valorCelda}");*/
+            CeldaControl celdaAuxiliar = new CeldaControl("", "edi", NumFila);
+            // TO DO: optimizar con una instruccion de predicados
+            bool edicionDuplicada = false;
+            foreach(CeldaControl celda in celdas)
+            {
+                if (celda.IdElemento.Equals(celdaAuxiliar.IdElemento)
+                    && celda.Operacion.Equals(celdaAuxiliar.Operacion))
+                    edicionDuplicada = true;
+            }
+
+            if (!edicionDuplicada)
+            {
+                celdas.Add(celdaAuxiliar);
+                //e.Row.Background = Brushes.Yellow;
+                bt_aplicarCambiosReg.IsEnabled = true;
+
+            }
+
+
+
         }
 
         private void dg_Usuarios_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
@@ -177,6 +202,81 @@ namespace Monitor_de_salas_de_computo
             usuORM.Actualizar(_usuarios.ElementAt(e.Row.GetIndex()));
             */
 
+        }
+
+        private void dg_Registros_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Subtract || e.Key == Key.Delete)
+            {
+                
+                int numFila = dg_Registros.SelectedIndex;
+                CeldaControl celdaAuxiliar = new CeldaControl("", "eli", numFila);
+                // TO DO: optimizar con una instruccion de predicados
+                bool edicionDuplicada = false;
+                foreach (CeldaControl celda in celdas)
+                {
+                    if (celda.IdElemento.Equals(celdaAuxiliar.IdElemento)
+                        && celda.Operacion.Equals(celdaAuxiliar.Operacion))
+                        edicionDuplicada = true;
+                }
+
+                if (!edicionDuplicada)
+                {
+                    celdas.Add(celdaAuxiliar);
+                    //  = Brushes.Red;
+                    bt_aplicarCambiosReg.IsEnabled = true;
+                }
+            }
+        }
+
+        private void bt_aplicarCambiosReg_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (CeldaControl celda in celdas)
+            {
+                RegistroORM operaciones = new RegistroORM();
+                switch (celda.Operacion)
+                {
+                    case "edi":
+                        operaciones.Actualizar(controlador.Registros.ElementAt<Registro>(celda.IdElemento));
+                        ActualizarDatos();
+                        break;
+
+                    case "eli":
+                        operaciones.Eliminar(controlador.Registros.ElementAt<Registro>(celda.IdElemento));
+                        ActualizarDatos();
+                        break;
+
+                    case "cre":
+                        /*operaciones.Insertar(new Registro(1, 1, 1, new DateTime(), "Mendez", "Martinez", "MarAdmin", "MarAdmin", "1478522", "email@gmail.com"
+                            , "0", "ICO", new DateTime(2016, 03, 04), new DateTime(2000, 10, 14)));
+                        controlador.ActualizarRegistros();*/
+                        break;
+                }
+            }
+
+            celdas.Clear();
+            bt_aplicarCambiosReg.IsEnabled = false;
+        }
+
+        private void dg_Registros_AddingNewItem(object sender, AddingNewItemEventArgs e)
+        {
+            int numFila = -1;
+            CeldaControl celdaAuxiliar = new CeldaControl("", "cre", numFila);
+            // TO DO: optimizar con una instruccion de predicados
+            bool edicionDuplicada = false;
+            foreach (CeldaControl celda in celdas)
+            {
+                if (celda.IdElemento.Equals(celdaAuxiliar.IdElemento)
+                    && celda.Operacion.Equals(celdaAuxiliar.Operacion))
+                    edicionDuplicada = true;
+            }
+
+            if (!edicionDuplicada)
+            {
+                celdas.Add(celdaAuxiliar);
+                //  = Brushes.Red;
+                bt_aplicarCambiosReg.IsEnabled = true;
+            }
         }
     }
 }
--- Monitor de salas de computo/Controladores/CeldaControl.cs
+++ Monitor de salas de computo/Controladores/CeldaControl.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Monitor_de_salas_de_computo.Modelo;
 
 namespace Monitor_de_salas_de_computo.Controladores
 {
     class CeldaControl
     {
-        public string DatoOriginal { get; set; }
+        public object DatoOriginal { get; set; }
         public string Operacion { get; set; }
         /* 
          * Codigo de operaciones:
@@ -16,7 +17,35 @@ namespace Monitor_de_salas_de_computo.Controladores
          */
         public int IdElemento { get; }
 
-        public CeldaControl(string datoOriginal, string operacion, int idElemento)
+        public CeldaControl(Registro datoOriginal, string operacion, int idElemento)
+        {
+            DatoOriginal = datoOriginal;
+            Operacion = operacion;
+            IdElemento = idElemento;
+        }
+
+        public CeldaControl(Usuario datoOriginal, string operacion, int idElemento)
+        {
+            DatoOriginal = datoOriginal;
+            Operacion = operacion;
+            IdElemento = idElemento;
+        }
+
+        public CeldaControl(Sala datoOriginal, string operacion, int idElemento)
+        {
+            DatoOriginal = datoOriginal;
+            Operacion = operacion;
+            IdElemento = idElemento;
+        }
+
+        public CeldaControl(Computadora datoOriginal, string operacion, int idElemento)
+        {
+            DatoOriginal = datoOriginal;
+            Operacion = operacion;
+            IdElemento = idElemento;
+        }
+
+        public CeldaControl(Configuraciones datoOriginal, string operacion, int idElemento)
         {
             DatoOriginal = datoOriginal;
             Operacion = operacion;
